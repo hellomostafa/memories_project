82b3ef732787499ef8139cd0a201a050bf3ddafd
amir <iamamirmostafa@gmail.com>
2021-09-18 19:16:08 +0600

    update s

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import Mongoose from 'mongoose';
+import Mongoose from 'mongoose'
 
 import postRoutes from './routes/posts.js';
 
